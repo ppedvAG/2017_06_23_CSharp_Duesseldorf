38b8f31bab5fbc27004e1255c0285e146b802e4e
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-06-26 10:37:48 +0200

    Grundlagen

--- HalloCSharp/Program.cs
+++ HalloCSharp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HalloCSharp
 {
@@ -10,6 +6,62 @@ namespace HalloCSharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hallo Welt!");
+
+            string text = "Mein wichtiger Text";
+            var andererText = "laksdjfölaksjf";
+
+            byte einByte = 255;
+            short s = 32000;
+            int x = 5;
+            long l = 90000000000000;
+
+            float f = 7.98f;
+            double d = 2.12;
+            decimal dec = 7.34m;
+
+            char c = '\'';
+
+            bool b = true;      // false
+
+            /*
+             
+             Ein langes Kommentar
+             
+             */
+
+
+            // Mathematische Operatoren
+            x = 8 + 9;
+            x = 8 - 9;
+            x = 8 * 9;
+            x = 13 / 5;
+            x = 13 % 5;
+
+            // Vergleichsoperatoren
+            b = 7 < 4;
+            b = 7 <= 4;
+            b = 7 > 4;
+            b = 7 >= 4;
+            b = 7 == 4;
+            b = 7 != 4;
+            b = !(5 < 3);
+
+            // Verknüpfungsoperatoren
+            b = (6 < 7) & (4 > 4);
+            b = (6 < 7) | (4 > 4);
+            b = (6 < 7) ^ (4 > 4);
+
+            b = (6 < 7) && (4 > 4);
+            b = (6 < 7) || (4 > 4);
+
+
+            var eingabe = Console.ReadLine();
+
+            x = Convert.ToInt32(eingabe);
+            x = int.Parse(eingabe, System.Globalization.NumberStyles.HexNumber, new System.Globalization.CultureInfo("de-DE"));
+
+            Console.ReadKey();
         }
     }
 }
