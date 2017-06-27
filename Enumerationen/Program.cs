a8b011e31155670e7cd7fc65335106a9e13adc06
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-06-27 14:37:45 +0200

    Enums

---
+++ Enumerationen/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Enumerationen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var result = MessageBox.Show(
+                    text: "Bitte wenden Sie sich umgehend an Ihren Systemadministrator!",
+                    caption: "Unbekannter Fehler!",
+                    buttons: MessageBoxButtons.RetryCancel,
+                    icon: MessageBoxIcon.Stop);
+
+                switch (result)
+                {
+                    case DialogResult.Cancel:
+                        return;
+                    case DialogResult.Retry:
+                        Console.WriteLine("Nochmal nochmal.");
+                        break;
+                }
+            }
+
+            var tage = Wochentage.Montag | Wochentage.Freitag;
+
+            Console.WriteLine(tage);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Enumerationen/Wochentage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enumerationen
+{
+    [Flags]
+    public enum Wochentage : byte
+    {
+        Montag = 1<<0,
+        Dienstag = 1<<1,
+        Mittwoch = 1<<2,
+        Donnerstag = 1<<3,
+        Freitag = 1<<4,
+        Samstag = 1<<5,
+        Sonntag = 1<<6
+    }
+}
