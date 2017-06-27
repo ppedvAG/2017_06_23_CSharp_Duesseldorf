5476386ae6a75ef6c563c981a8148b795f1492b1
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-06-27 11:48:07 +0200

    Abstrakte Klassen

---
+++ AbstrakteKlassen/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AbstrakteKlassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var l = new Linie { Farbe = "Blau", Länge = 7 };
+            var k = new Kreis { Farbe = "Grün", Radius = 2.12 };
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            l.Zeichnen();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            k.Zeichnen();
+
+            Console.WriteLine("\n");
+            Console.ForegroundColor = ConsoleColor.Black;
+            MachWasMitEinerGrafik(l);
+            MachWasMitEinerGrafik(k);
+
+            Console.ReadKey();
+        }
+
+        private static void MachWasMitEinerGrafik(Grafik g)
+        {
+            g.Zeichnen();
+        }
+    }
+}
