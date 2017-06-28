d19ca4958538c91e9196a444ebd048ceee3f7d5b
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-06-28 10:47:42 +0200

    Operatoren Überladen

---
+++ OperatorenÜberladen/Bruchzahl.cs
@@ -0,0 +1,79 @@
+﻿namespace OperatorenÜberladen
+{
+    public struct Bruchzahl
+    {
+        public int Zähler { get; }
+        public int Nenner { get; }
+
+        public Bruchzahl(int zähler, int nenner)
+        {
+            Zähler = zähler;
+            Nenner = nenner;
+        }
+        
+        public static Bruchzahl operator *(Bruchzahl left, Bruchzahl right)
+        {
+            return new Bruchzahl(
+                zähler: left.Zähler * right.Zähler,
+                nenner: left.Nenner * right.Nenner).Kürzen();
+        }
+        public static Bruchzahl operator *(Bruchzahl left, int right)
+        {
+            return new Bruchzahl(
+                zähler: left.Zähler * right,
+                nenner: left.Nenner).Kürzen();
+        }
+        public static Bruchzahl operator *(int left, Bruchzahl right)
+        {
+            return right * left;
+        }
+        public static bool operator ==(Bruchzahl left, Bruchzahl right)
+        {
+            return left.Zähler == right.Zähler &&
+                   left.Nenner == right.Nenner;
+        }
+        public static bool operator !=(Bruchzahl left, Bruchzahl right)
+        {
+            return left.Zähler != right.Zähler ||
+                   left.Nenner != right.Nenner;
+        }
+
+        public Bruchzahl Kürzen()
+        {
+            var größterGemeinsamerTeiler = GrößterGemeinsamerTeiler(Zähler, Nenner);
+
+            return new Bruchzahl(
+                zähler: Zähler / größterGemeinsamerTeiler,
+                nenner: Nenner / größterGemeinsamerTeiler);
+
+            int GrößterGemeinsamerTeiler(int z1, int z2)
+            {
+                if (z2 == 0)
+                {
+                    return z1;
+                }
+                else
+                {
+                    return GrößterGemeinsamerTeiler(z2, z1 % z2);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Zähler}/{Nenner}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Bruchzahl b)
+                return this == b;
+
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ OperatorenÜberladen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OperatorenÜberladen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b1 = new Bruchzahl(30, 6);
+            var b2 = new Bruchzahl(2, 3);
+
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+
+            var ergebnis = b1 * b2;
+
+            Console.WriteLine(ergebnis);
+            Console.ReadKey();
+        }
+    }
+}
